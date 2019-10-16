c9d6646b001fba4aefd2de5eaf8bb16ca3094c6e
Mike De Wolfe <mdewolfe@spiria.com>
2019-10-16 13:17:01 -0400

    DA Tracking

--- js/acmethunder.js
+++ js/acmethunder.js
@@ -97,4 +97,5 @@ $(document).ready(function() {
     let index = Math.floor(Math.random() * QUOTES.length);
     $('#main-content').append(`<h1>${QUOTES[index].text}</h1>`);
     $('#main-content').append(`<h3>- ${QUOTES[index].credit}</h3>`);
+    ga('send', 'event', 'quote', 'display', QUOTES[index].credit, index);
 });
